f0876a3a35b3b49d592fc0e734172c05b756c4fb
nikShri31 <nimanshushrivastav7172@gmail.com>
2024-05-15 10:55:43 +0530

    final changes

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -55,7 +55,7 @@ export class AuthService {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite serive :: getCurrentUser :: error", error);
+            console.log("Appwrite service :: getCurrentUser :: error", error);
         }
 
         return null;
@@ -66,7 +66,7 @@ export class AuthService {
         try {
             await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite serive :: logout :: error", error);
+            console.log("Appwrite service :: logout :: error", error);
         }
     }
 }
